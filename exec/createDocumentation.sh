066a41f93ba0ac9fae48790d7d8992edb8868d4e
eahrne <ahrnee-adm@bz-bzas03-plm05.bioz.unibas.ch>
2016-01-06 11:39:06 +0100

    remove sh scripts

--- exec/createDocumentation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-Rscript /Users/ahrnee-adm/dev/R/workspace/SafeQuant/exec/roxygenize.R
-R CMD Rd2pdf ../../SafeQuant --output=/Users/ahrnee-adm/dev/R/workspace/SafeQuant/inst/manuals/SafeQuant-man.pdf --force --no-preview
