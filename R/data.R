f89f985d115b252af5f726706e794440baff20e5
eahrne <ahrnee-adm@bz-bzas03-plm05.bioz.unibas.ch>
2017-02-13 14:24:36 +0100

    kinaseMotif

--- R/data.R
+++ R/data.R
@@ -11,29 +11,28 @@
 #' }
 "kinaseMotif"
 
-#if(F){
-	# library(magrittr)
-	# 
-	#  #kinaseMotif =  read.csv(file="~/dev/R/workspace/CristinaPhospho/data/motifsEtc/HPRD_kinas_motifs.csv")
-	#  kinaseMotif =  read.csv(file="~/dev/R/workspace/CristinaPhospho/data/motifsEtc/HPRD_kinas_motifs_curated.csv",sep="\t")
-	#  names(kinaseMotif) = c("motif","kinase","binding")
-	#  # discard double phoshpho e.g. pS*PXX[pS/pT]
-	#  selDoubleS = lapply(kinaseMotif$motif,  function(t){  gregexpr("pS",t)} %>%  unlist %>% length   ) %>% unlist %>% as.vector > 1
-	#  selDoubleT = lapply(kinaseMotif$motif,  function(t){  gregexpr("pT",t)} %>%  unlist %>% length   ) %>% unlist %>% as.vector > 1
-	#  kinaseMotif = kinaseMotif[!(selDoubleS | selDoubleT | grepl("\\*",kinaseMotif$motif) ),]
-	#  kinaseMotif$regExpr = kinaseMotif$motif %>% gsub(("X"),".",.) %>% gsub("(p)([STY])","(p\\2)",.) %>%  gsub("\\)\\/\\(",")|(",.) %>%  gsub("\\[\\(","((",.)  %>%  gsub("\\)\\]","))",.) %>% gsub("\\/","",.)
-	#  kinaseMotif$kinase = gsub("Kinase","kinase",kinaseMotif$kinase)
-	#  kinaseMotif = unique(kinaseMotif)
-	#  save(kinaseMotif, file="~/dev/R/workspace/SafeQuant/data/kinaseMotif.rda")
-	# # grepl("(pS)|(pT)","XXXpSXXX", ignore.case = FALSE)
-	# # grepl("KKKKKK((pS)|(pT)).","KKKKKKpTX")
-	# # grepl("R.R..((pS)|(pT))[FL]","RXRXXpSL")
-	# # grepl("R.R..((pS)|(pT))[FL]X","RXRXXpSL")
-	# # grepl("R.R..((pS)|(pT))[FL]","RXRXXpTLX")
-	# # grepl("...","KK")
-	# 
-	# lapply(kinaseMotif$regExpr,function(t){grepl(t,"AAA")})
-
+if(F){
+	 library(magrittr)
+	 
+	  #kinaseMotif =  read.csv(file="~/dev/R/workspace/CristinaPhospho/data/motifsEtc/HPRD_kinas_motifs.csv")
+	  kinaseMotif =  read.csv(file="~/dev/R/workspace/CristinaPhospho/data/motifsEtc/HPRD_kinas_motifs_curated.csv",sep="\t")
+	  names(kinaseMotif) = c("motif","kinase","binding")
+	  # discard double phoshpho e.g. pS*PXX[pS/pT]
+	  selDoubleS = lapply(kinaseMotif$motif,  function(t){  gregexpr("pS",t)} %>%  unlist %>% length   ) %>% unlist %>% as.vector > 1
+	  selDoubleT = lapply(kinaseMotif$motif,  function(t){  gregexpr("pT",t)} %>%  unlist %>% length   ) %>% unlist %>% as.vector > 1
+	  kinaseMotif = kinaseMotif[!(selDoubleS | selDoubleT | grepl("\\*",kinaseMotif$motif) ),]
+	  kinaseMotif$regExpr = kinaseMotif$motif %>% gsub(("X"),".",.) %>% gsub("(p)([STY])","(p\\2)",.) %>%  gsub("\\)\\/\\(",")|(",.) %>%  gsub("\\[\\(","((",.)  %>%  gsub("\\)\\]","))",.) %>% gsub("\\/","",.)
+	  kinaseMotif$kinase = gsub("Kinase","kinase",kinaseMotif$kinase)
+	  kinaseMotif = unique(kinaseMotif)
+	  save(kinaseMotif, file="~/dev/R/workspace/SafeQuant/data/kinaseMotif.rda")
+	 # grepl("(pS)|(pT)","XXXpSXXX", ignore.case = FALSE)
+	 # grepl("KKKKKK((pS)|(pT)).","KKKKKKpTX")
+	 # grepl("R.R..((pS)|(pT))[FL]","RXRXXpSL")
+	 # grepl("R.R..((pS)|(pT))[FL]X","RXRXXpSL")
+	 # grepl("R.R..((pS)|(pT))[FL]","RXRXXpTLX")
+	 # grepl("...","KK")
+	 
+	 #lapply(kinaseMotif$regExpr,function(t){grepl(t,"AAA")})
 	#kinaseMotif[kinaseMotif$regExpr == '[ED(pT)|(pY)).(pY)EE',] 
 	
-#}
+}
